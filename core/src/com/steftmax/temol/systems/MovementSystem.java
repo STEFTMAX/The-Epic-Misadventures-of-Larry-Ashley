0bd25ebcb02eea953cbd4b30139212b23ccb3dd6
pieter3457 <pieteratmail@gmail.com>
2016-02-18 23:37:58 +0100

    missed in last commit so will commit now

--- core/src/com/steftmax/temol/systems/MovementSystem.java
+++ core/src/com/steftmax/temol/systems/MovementSystem.java
@@ -4,6 +4,7 @@ import com.badlogic.ashley.core.ComponentMapper;
 import com.badlogic.ashley.core.Entity;
 import com.badlogic.ashley.core.Family;
 import com.badlogic.ashley.systems.IteratingSystem;
+import com.badlogic.gdx.math.Vector2;
 import com.steftmax.temol.component.PositionComponent;
 import com.steftmax.temol.component.VelocityComponent;
 
@@ -11,21 +12,24 @@ import com.steftmax.temol.component.VelocityComponent;
  * @author pieter3457
  *
  */
-@Deprecated
 public class MovementSystem extends IteratingSystem {
+	
 
 	private ComponentMapper<PositionComponent> pm = ComponentMapper.getFor(PositionComponent.class);
 	private ComponentMapper<VelocityComponent> vm = ComponentMapper.getFor(VelocityComponent.class);
-
+	
 	public MovementSystem() {
 		super(Family.all(PositionComponent.class, VelocityComponent.class).get());
 	}
-
-	public void processEntity(Entity entity, float deltaTime) {
-		PositionComponent position = pm.get(entity);
-		VelocityComponent velocity = vm.get(entity);
-
-		position.x += velocity.x * deltaTime;
-		position.y += velocity.y * deltaTime;
+	
+	/* (non-Javadoc)
+	 * @see com.badlogic.ashley.systems.IteratingSystem#processEntity(com.badlogic.ashley.core.Entity, float)
+	 */
+	@Override
+	protected void processEntity(Entity entity, float deltaTime) {
+		
+		final Vector2 velocity = vm.get(entity).velocity;
+		
+		pm.get(entity).position.add(velocity.x*deltaTime, velocity.y*deltaTime);
 	}
 }
