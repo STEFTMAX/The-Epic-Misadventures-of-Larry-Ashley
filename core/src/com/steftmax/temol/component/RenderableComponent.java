73705b59be43e75384abe1bf60bd7567f8293eb1
pieter3457 <pieteratmail@gmail.com>
2016-02-24 12:29:41 +0100

    create RenderableComponent

---
+++ core/src/com/steftmax/temol/component/RenderableComponent.java
@@ -0,0 +1,20 @@
+package com.steftmax.temol.component;
+
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+/**
+ * @author pieter3457
+ *
+ */
+public class RenderableComponent {
+
+	public TextureRegion region;
+
+	public RenderableComponent(TextureRegion region) {
+		this.region = region;
+	}
+
+	public RenderableComponent() {
+
+	}
+}
